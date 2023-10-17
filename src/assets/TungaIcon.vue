b9cbbd8f9e5bb8245725cb0fec4639d1df2f14b4
David Nene <64870164+davidnene@users.noreply.github.com>
2023-10-17 22:47:00 +0300

    Add Tunga icon component

---
+++ src/assets/TungaIcon.vue
@@ -0,0 +1,10 @@
+<script setup>
+    import { Icon } from '@iconify/vue';
+</script>
+<template>
+    <Icon icon="fad:logo-audiobus" color="purple" width="300" />
+</template>
+
+<style lang="scss" scoped>
+
+</style>
