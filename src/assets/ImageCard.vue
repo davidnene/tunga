0e5efd274b01eace68852194bd401721ceac613b
David Nene <64870164+davidnene@users.noreply.github.com>
2023-10-24 11:45:27 +0300

    Fix icon attributes rendering from props

--- src/assets/ImageCard.vue
+++ src/assets/ImageCard.vue
@@ -14,10 +14,11 @@
 <template>
     <div>
         <div class="card body-padding" style="width: 18rem;">
-            <Icon icon="ic:baseline-corporate-fare" color="darkblue" width="80" />
+            <Icon :icon= "img.icon" :style="{color: img.style.color}" :width="img.width"/>
             <div class="card-body ">
                 <h4>{{ name }}</h4>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#">Learn more..</a>
             </div>
         </div>
     </div>
