e7bf0a92b37fe4e6e652b2e0f912056eaab5eacc
David Nene <64870164+davidnene@users.noreply.github.com>
2023-10-24 11:47:39 +0300

    Configure data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,25 +8,66 @@ import ImageCard from '../assets/ImageCard.vue'
 const data = [
   {
     "projectName": "LLMs in Health Care",
-    "projectImage": 'data:image/svg+xml,%3Csvg xmlns="http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg" width="80" height="80" viewBox="0 0 24 24"%3E%3Cpath fill="darkblue" d="M12 7V3H2v18h20V7H12zm-2 12H4v-2h6v2zm0-4H4v-2h6v2zm0-4H4V9h6v2zm0-4H4V5h6v2zm10 12h-8V9h8v10zm-2-8h-4v2h4v-2zm0 4h-4v2h4v-2z"%2F%3E%3C%2Fsvg%3E',
+    "projectImage": {
+      "icon": "ic:baseline-corporate-fare",
+      "style": {
+        "color": "darkblue",
+      },
+      "width": "150",
+    },
     "projectLink" : ""
   },
 
   {
     "projectName": "LLMs in Agriculture",
-    "projectImage": "",
+    "projectImage": {
+      "icon": "ic:baseline-spa",
+      "style": {
+        "color": "darkblue",
+      },
+      "width": "150",
+    },
     "projectLink" : ""
   },
   {
     "projectName": "ASR in Linguistics",
-    "projectImage": "",
+    "projectImage": {
+      "icon": "ic:baseline-forum",
+      "style": {
+        "color": "darkblue",
+      },
+      "width": "150",
+    },
+    "projectLink" : ""
+  },
+  
+  {
+    "projectName": "ML in Security",
+    "projectImage": {
+      "icon": "ic:baseline-gpp-good",
+      "style": {
+        "color": "darkblue",
+      },
+      "width": "150",
+    },
+    "projectLink" : ""
+  },
+  {
+    "projectName": "ML in Finance",
+    "projectImage": {
+      "icon": "ic:baseline-euro",
+      "style": {
+        "color": "darkblue",
+      },
+      "width": "150",
+    },
     "projectLink" : ""
   },
 ]
 </script>
 
 <template>
-  <main>
+  <main class="cust-padding">
     <TheWelcome tunga="Tunga" />
     <Quote/>
     <div class="container overflow-hidden text-center">
