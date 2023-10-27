fb3a1b506550590d59c3ca8d6ac93e3ae3d12592
David Nene <64870164+davidnene@users.noreply.github.com>
2023-10-27 18:36:38 +0300

    Add dashboard component

---
+++ src/components/Dash.vue
@@ -0,0 +1,37 @@
+<script setup>
+    defineProps({
+        author: {
+            type: String,
+            required: true,
+        },
+        role: {
+            type: String,
+            required: true,
+        },
+        link: {
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+    })
+</script>
+
+<template>
+    <div>
+        <div class="text-center">
+        <h4>{{ title }}</h4>
+        <iframe width="600" height="450" :src = "link" style="border:0" allowfullscreen></iframe>
+        <h6>Author: {{ author }}</h6>
+        <h6>Role: {{ role }}</h6>
+    </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.dash-outline {
+    border-color: darkblue;
+    border: 10px;
+}
+</style>
