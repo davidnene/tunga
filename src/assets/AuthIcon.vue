02980772329f5bc4cd47a082f0cd0ae65c654e6a
David Nene <64870164+davidnene@users.noreply.github.com>
2023-10-27 12:02:30 +0300

    Add Auth icon

---
+++ src/assets/AuthIcon.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <Icon icon="bi:person-lock" color="grey" width="300" />
+    </div>
+</template>
+
+<script setup>
+import { Icon } from '@iconify/vue';
+</script>
+
+<style lang="scss" scoped>
+
+</style>
