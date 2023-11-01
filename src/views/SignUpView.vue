144082d5d6c691c49abe0bc527a0f54c80c74543
David Nene <64870164+davidnene@users.noreply.github.com>
2023-11-01 12:25:14 +0300

    Configure signup with api

--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,5 +1,33 @@
 <script setup>
 import AuthIcon from '../assets/AuthIcon.vue';
+import { ref, reactive } from 'vue';
+import axios from 'axios';
+import router from '../router/index';
+
+const signUpData = reactive({
+    name: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
+});
+
+const signUpError = ref('')
+
+const sendSignup = async() => {
+    try {
+        const response = await axios.post("http://localhost:8000/api/register", {
+            name: signUpData.name,
+            email: signUpData.email,
+            password: signUpData.password,
+            password_confirmation: signUpData.password_confirmation
+        });
+        router.push('/login');
+    } catch (error) {
+        console.error("Error signing up: ",error)
+        signUpError.value = error.response.data.errors;
+    }
+}
+
 </script>
 
 <template>
@@ -10,16 +38,28 @@ import AuthIcon from '../assets/AuthIcon.vue';
             <AuthIcon/>
         </div>
         <div class="col-md-6 justify-content-center">
-        <form class="row g-3">
-            <div class="col-md-6">
-                <label for="inputEmail4" class="form-label">Email</label>
-                <input type="email" class="form-control" id="inputEmail4">
+        <form class="row g-3" @submit.prevent=sendSignup>
+            <div class="col-12">
+                <label for="inputName" class="form-label">Name</label>
+                <input type="text" class="form-control" id="inputName" placeholder="Full name" v-model="signUpData.name">
             </div>
+            <div class="col-md-12">
+                <label for="inputEmail4" class="form-label">Email</label>
+                <input type="email" class="form-control" id="inputEmail4" v-model="signUpData.email">
+            </div>
+                <p v-if="signUpError.email !== ''">{{ signUpError.email }}</p>
             <div class="col-md-6">
                 <label for="inputPassword4" class="form-label">Password</label>
-                <input type="password" class="form-control" id="inputPassword4">
+                <input type="password" class="form-control" id="inputPassword4" v-model="signUpData.password">
             </div>
-            <div class="col-12">
+            <div class="col-md-6">
+                <label for="inputPassword4" class="form-label">Confirm Password</label>
+                <input type="password" class="form-control" id="inputPasswordConfirm4" v-model="signUpData.password_confirmation">
+            </div>
+            <div>
+                <p v-show="signUpError.password !== ''">{{ signUpError.password }}</p>
+            </div>
+            <!-- <div class="col-12">
                 <label for="inputAddress" class="form-label">Address</label>
                 <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
             </div>
@@ -49,7 +89,7 @@ import AuthIcon from '../assets/AuthIcon.vue';
                     Check me out
                 </label>
                 </div>
-            </div>
+            </div> -->
             <div>
                 <div class="row">
                     <div class="col-md-4" style="color: white;">
